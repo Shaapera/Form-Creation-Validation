0d5df04273cecbdc920c154f92fab077469bd10b
Shaapera <shalomshaapera@gmail.com>
2025-01-25 00:15:00 +0100

    tasks 7

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    ("Page Loaded");
+});
+const form = document.getElementById("registration-form").addEventListener('submit', function (event) {
+    event.preventDefault()
+} );
+const feedbackDiv = document.getElementById("form-feedback");
+const username = document.getElementById('username').value.trim();
+const email= document.getElementById('email').value.trim();
+const password = document.getElementById('password').value.trim();
+const isvalid = true;
+const message = [];
+if (username.length < 3) {
+    isvalid == false;
+    message.push('give a user name');
+};
+console.log(message);
+
+if (!email.includes('@') && includes('.')) {
+    isvalid == false;
+    message.push('your email is required');
+};
+console.log(message);
+if (password.length >= 8) {
+    isvalid = false;
+    message.push('password must be more than 8 characters');
+}
+console.log(message);
